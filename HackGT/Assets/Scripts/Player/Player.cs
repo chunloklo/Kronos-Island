3e91c96d154d65c8ffa6bdf92692b23a6641baa8
Sabrina Ang <sabrina.ang@gatech.edu>
2017-10-13 21:20:44 -0400

    Start on Player controls
    made some folders
    started player script

---
+++ HackGT/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Image damageImage;
+    public Slider healthSlider;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    bool isDead;
+    bool damaged;
+
+    void Awake ()
+    {
+        currentHealth = startingHealth;
+    }
+
+    void Update ()
+    {
+        if(damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+    }
+
+}
