9be46483479d962064450b3ad72c59f826c78634
Sabrina Ang <sabrina.ang@gatech.edu>
2017-10-13 23:54:49 -0400

    fps movement works

--- HackGT/Assets/Scripts/Player/PlayerController.cs
+++ HackGT/Assets/Scripts/Player/PlayerController.cs
@@ -2,12 +2,39 @@
 
 public class PlayerController : MonoBehaviour
 {
+    //void Update()
+    //{
+    //    var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+    //    var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+    //    transform.Rotate(0, x, 0);
+    //    transform.Translate(0, 0, z);
+    //}
+
+    public float walkSpeed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float runSpeed = 8.0F;
+    public float gravity = 20.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= walkSpeed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
