4e58242fff7497239f708115d45ec55743d999a9
Sabrina Ang <sabrina.ang@gatech.edu>
2017-10-13 23:11:49 -0400

    health bar works

--- HackGT/Assets/Scripts/Player/Player.cs
+++ HackGT/Assets/Scripts/Player/Player.cs
@@ -1,41 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-    public int startingHealth = 100;
-    public int currentHealth;
-    public Image damageImage;
-    public Slider healthSlider;
-    public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    GameObject player;
 
-    bool isDead;
-    bool damaged;
-
-    void Awake ()
-    {
-        currentHealth = startingHealth;
-    }
-
-    void Update ()
-    {
-        if(damaged)
-        {
-            damageImage.color = flashColour;
-        }
-        else
-        {
-            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
-        damaged = false;
-    }
-
-    public void TakeDamage (int amount)
-    {
-        damaged = true;
-    }
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
 }
---
+++ HackGT/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Image damageImage;
+    public Slider healthSlider;
+    public float flashSpeed = 5f;
+    public Color flashColour = Color.red;
+
+    bool isDead;
+    bool damaged;
+
+    void Awake ()
+    {
+        currentHealth = startingHealth;
+    }
+
+    void Update ()
+    {
+        if(damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        //make menu pop up
+        //restart game
+    }
+}
